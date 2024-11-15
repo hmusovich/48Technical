e3e80ed3daf8e5a4c0adc4fe321a76c92447e59b
Hanan Musovich <hmusovich@gmail.com>
2024-11-15 13:46:26 +1300

    151124 A

---
+++ contact/send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Set the email parameters
+    $to = "hmusovich@gmail.com"; // Replace with your email address
+    $subject = "+48 Technical Contact Form";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message sending failed!";
+    }
+}
+?>
